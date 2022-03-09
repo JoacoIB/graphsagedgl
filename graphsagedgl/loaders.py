c0f13ecb38a0c7a3d6e216ec1025489a7788f790
Joaquin Barotto <joaquinbarotto@gmail.com>
2022-03-09 18:10:29 -0300

    Add features to graph loader

--- graphsagedgl/loaders.py
+++ graphsagedgl/loaders.py
@@ -1,5 +1,7 @@
 import json
 import dgl
+import torch
+import torchtext
 
 
 class JSONDatasetLoader(object):
@@ -55,7 +57,42 @@ class JSONDatasetLoader(object):
             )
         return self.graph
 
+    def add_features(self, feature_names):
+        features = {feature_name: [] for feature_name in feature_names}
+        with open(self.path, 'r') as f:
+            for line in f:
+                data = json.loads(line)
+                for feature_name in feature_names:
+                    features[feature_name].append(data[feature_name])
+        for feature_name in features:
+            first_val = features[feature_name][0]
+            if isinstance(first_val, int):
+                self.graph.edges[self.a2b_type].data[feature_name] = torch.IntTensor(features[feature_name])
+                self.graph.edges[self.b2a_type].data[feature_name] = torch.IntTensor(features[feature_name])
+            elif isinstance(first_val, float):
+                self.graph.edges[self.a2b_type].data[feature_name] = torch.FloatTensor(features[feature_name])
+                self.graph.edges[self.b2a_type].data[feature_name] = torch.FloatTensor(features[feature_name])
+            elif isinstance(first_val, str):
+                tokenizer = torchtext.data.utils.get_tokenizer("basic_english")
+                tokenized = [tokenizer(line) for line in features[feature_name]]
+                vocab = torchtext.vocab.build_vocab_from_iterator(tokenized)
+                numericalized = []
+                max_len = 0
+                for x in features[feature_name]:
+                    numericalized.append([vocab[token] for token in tokenizer(x)])
+                    if len(numericalized[-1]) > max_len:
+                        max_len = len(numericalized[-1])
+                padded = torch.zeros(len(numericalized), max_len)
+                for i, x in enumerate(numericalized):
+                    padded[i, :len(x)] = torch.IntTensor(x)
+                self.graph.edges[self.a2b_type].data[feature_name] = padded
+                self.graph.edges[self.b2a_type].data[feature_name] = padded
+
 
 class RedditDatasetLoader(JSONDatasetLoader):
     def __init__(self, path):
         super().__init__(path, 'author', 'subreddit', 'posted_to', 'posted_by')
+
+    def add_reddit_features(self):
+        feature_names = ['body']
+        self.add_features(feature_names)
--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -1,9 +1,9 @@
 import unittest
-from graphsagedgl.loaders import RedditDatasetLoader
+from graphsagedgl.loaders import RedditDatasetLoader, JSONDatasetLoader
 
 
 class TestRedditDatasetLoader(unittest.TestCase):
-    def test_load_dataset(self):
+    def test_load_graph(self):
         loader = RedditDatasetLoader('tests/data/loader_data.json')
         g = loader.create_graph()
         self.assertEqual(
@@ -18,3 +18,19 @@ class TestRedditDatasetLoader(unittest.TestCase):
             g.number_of_edges(),
             10
         )
+
+    def test_load_reddit_features(self):
+        loader = RedditDatasetLoader('tests/data/loader_data.json')
+        _ = loader.create_graph()
+        loader.add_reddit_features()
+        self.assertEqual(loader.graph.edges[loader.a2b_type].data["body"].shape, (5, 4))
+        self.assertEqual(loader.graph.edges[loader.b2a_type].data["body"].shape, (5, 4))
+
+    def test_load_custom_features(self):
+        loader = JSONDatasetLoader('tests/data/loader_data.json', 'author', 'subreddit', 'posted_to', 'posted_by')
+        _ = loader.create_graph()
+        loader.add_features(["score", "rate"])
+        self.assertEqual(loader.graph.edges[loader.a2b_type].data["score"].shape, (5,))
+        self.assertEqual(loader.graph.edges[loader.b2a_type].data["score"].shape, (5,))
+        self.assertEqual(loader.graph.edges[loader.a2b_type].data["rate"].shape, (5,))
+        self.assertEqual(loader.graph.edges[loader.b2a_type].data["rate"].shape, (5,))
